bed902ee50f4257b9c617cc2bf1cd252b1673739
Cody Swendrowski <cody@swendrowski.us>
2020-06-21 21:00:36 -0500

    Version 1

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-npc-chatter
+![](https://img.shields.io/badge/Foundry-v0.6.2-informational)
+[![](https://img.shields.io/badge/Buy%20Me%20A%20Coffee-%243-orange)](https://www.buymeacoffee.com/T2tZvWJ)
+
+
+# NPC Chatter
+
+Your NPCs have things to say.
+This Module provides a small API that allows your NPCs to randomly chatter off of roll tables defined for them.
+
+Chatter is displayed as ChatBubbles only, and won't dump to the Chat Log.
+
+I recommend you turn off the Core "Pan to Token Speaker" setting or else risk whiplash.
+
+# Setup
+
+Given a set of Villagers named "Villager A", "Villager B", and "Villager C", setup a "Villager Chatter" roll table or a "Villager" table under a Folder named "NPC Chatter".
+
+This roll table will need at least one option and a dice roll configured to work.
+
+If "Villager A" has specific dialogue that might trigger on top of the generic Villager chatter, add another "Villager A Chatter" table (or "Villager A" in the "NPC Chatter folder) and it will draw from that as well.
+
+# Usage
+
+Chatter can only be triggered via scripting. There are a few Macros included as examples - most work out of the box, but some require additional setup.
+
+The easiest macro is the "Timed Global Chatter" macro - just slap it when a Scene Loads, and they will start chattering!
+
+## Trigger Happy
+
+[Trigger Happy](https://github.com/kakaroto/fvtt-module-trigger-happy) can trigger NPC chatter as well. Here's two examples to get you started:
+
+On Scene Load, start Timed Global Chatter: `@Scene[Village ground level] @Macro[xSKCgl6katupNxCx]{Timed Global Chatter}`
+
+When an Actor walks into a Room (defined by an invisble actor), have a specific Token chatter: `@Actor[TriggerA] @Macro[OXyjmVhEGo3eTaJz]{Specific Token Chatter}`
+
+# API
+
+## Global Chatter
+
+Picks a random Chatter Table belonging to a random Actor on the first active scene and displays a ChatBubble with rolled Text off of the random Chatter Table.
+
+```js
+async globalChatter()
+```
+
+## Global Chatter Every Interval
+
+Every interval as measured in milliseconds, executes `globalChatter()`.
+
+```js
+randomGlobalChatterEvery(milliseconds)
+```
+
+## Token Chatter
+
+Given a `Token`, tries to find a matching Chatter Table. If none, exits. If one or more, randomly picks one and displays a rolled result from it as a ChatBubble.
+
+```js
+async tokenChatter(token)
+```
+
+## Selected Chatter
+
+Grabs the currently selected Tokens and tries to find matching Chatter Tables. If none, exits. If one or more, randomly picks one and an elibable Token and displays a rolled result from the Table as a ChatBubble.
+
+```js
+  async selectedChatter()
+```
---
+++ npcchatter.js
@@ -0,0 +1,78 @@
+class NpcChatter {
+
+  getChatterTables() {
+    var chatterFolder =game.folders.entities.filter(x => x.type == "RollTable" && x.name.toLowerCase() == "npc chatter")[0];
+    var tables = game.tables.entities.filter(x => x.name.toLowerCase().endsWith("chatter") || x.data.folder == chatterFolder.id);
+    return tables;
+  }
+
+  randomGlobalChatterEvery(milliseconds) {
+    window.setInterval(() => { game.npcChatter.globalChatter(); }, milliseconds);
+  }
+
+  async globalChatter() {
+    var tables = this.getChatterTables();
+
+    var userCharacterActorIds = game.users.entities.filter(x => x.character).map(x => x.character.id);
+    var activeScene = game.scenes.filter(x => x.active)[0];
+    var npcTokens = activeScene.data.tokens.filter(x => !userCharacterActorIds.includes(x.actorId));
+
+    var eligableTables = tables.filter(x => npcTokens.filter(t => x.name.toLowerCase().includes(t.name.toLowerCase().replace("chatter", "").trim()) > 0));
+
+    var tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
+    var table = eligableTables[tableIndex];
+
+    var eligableTokens = npcTokens.filter(x => x.name.toLowerCase().includes(table.name.toLowerCase().replace("chatter", "").trim()));
+
+    var tokenIndex = Math.floor((Math.random() * eligableTokens.length) + 0);
+    var token = eligableTokens[tokenIndex];
+
+    var result = table.roll().results[0].text;
+    await canvas.hud.bubbles.say(token, result, false);
+  }
+
+  async tokenChatter(token) {
+    var tables = this.getChatterTables();
+
+    var eligableTables = tables.filter(x => token.name.toLowerCase().includes(x.name.toLowerCase().replace("chatter", "").trim()));
+
+    if (eligableTables.length == 0) return;
+
+    var tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
+    var table = eligableTables[tableIndex];
+
+    var result = table.roll().results[0].text;
+    await canvas.hud.bubbles.say(token, result, false);
+  }
+
+  async selectedChatter() {
+    var tables = this.getChatterTables();
+
+    var npcTokens = canvas.tokens.controlled;
+
+    var eligableTables = tables.filter(x => npcTokens.filter(t => x.name.toLowerCase().includes(t.name.toLowerCase().replace("chatter", "").trim()) > 0));
+
+    if (eligableTables.length == 0) return;
+
+    var tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
+    var table = eligableTables[tableIndex];
+
+    var eligableTokens = npcTokens.filter(x => x.name.toLowerCase().includes(table.name.toLowerCase().replace("chatter", "").trim()));
+
+    var tokenIndex = Math.floor((Math.random() * eligableTokens.length) + 0);
+    var token = eligableTokens[tokenIndex];
+
+    var result = table.roll().results[0].text;
+    await canvas.hud.bubbles.say(token, result, false);
+  }
+}
+
+Hooks.once('ready', async function() {
+  game.npcChatter = new NpcChatter();
+  console.log("Npc Chatter is now ready");
+});
+
+Hooks.on('chatBubble', async function(callerData, html, text, emote) {
+  // Fixes https://gitlab.com/foundrynet/foundryvtt/-/issues/3136
+  html[0].setAttribute("style", "left: " + callerData.x + "px;");
+});
