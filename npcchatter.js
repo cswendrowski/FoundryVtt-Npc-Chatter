ac3ebe5f898419e4271a8e4e1014fc1dfe9c7277
Cody Swendrowski <toon324@gmail.com>
2021-05-24 09:48:11 -0500

    0.8.x compat

--- npcchatter.js
+++ npcchatter.js
@@ -3,8 +3,8 @@ class NpcChatter {
   static timer;
 
   getChatterTables() {
-    var chatterFolder =game.folders.entities.filter(x => x.type == "RollTable" && x.name.toLowerCase() == "npc chatter")[0];
-    var tables = game.tables.entities.filter(x => x.name.toLowerCase().endsWith("chatter") || x.data.folder == chatterFolder.id);
+    const chatterFolder = game.folders.contents.filter(x => x.type == "RollTable" && x.name.toLowerCase() == "npc chatter")[0];
+    const tables = game.tables.contents.filter(x => x.name.toLowerCase().endsWith("chatter") || x.data.folder == chatterFolder._id);
     return tables;
   }
 
@@ -13,68 +13,68 @@ class NpcChatter {
   }
 
   async globalChatter() {
-    var tables = this.getChatterTables();
+    const tables = this.getChatterTables();
 
-    var userCharacterActorIds = game.users.entities.filter(x => x.character).map(x => x.character.id);
-    var activeScene = game.scenes.filter(x => x.active)[0];
-    var npcTokens = activeScene.data.tokens.filter(x => !userCharacterActorIds.includes(x.actorId));
+    const userCharacterActorIds = game.users.contents.filter(x => x.character).map(x => x.character.id);
+    const activeScene = game.scenes.filter(x => x.active)[0];
+    const npcTokens = activeScene.data.tokens.filter(x => !userCharacterActorIds.includes(x.actorId));
 
-    var eligableTables = tables.filter(x => npcTokens.filter(t => x.name.toLowerCase().includes(t.name.toLowerCase().replace("chatter", "").trim()) > 0));
+    const eligableTables = tables.filter(x => npcTokens.filter(t => x.name.toLowerCase().includes(t.name.toLowerCase().replace("chatter", "").trim()) > 0));
 
-    var tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
-    var table = eligableTables[tableIndex];
+    const tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
+    const table = eligableTables[tableIndex];
 
-    var eligableTokens = npcTokens.filter(x => x.name.toLowerCase().includes(table.name.toLowerCase().replace("chatter", "").trim()));
+    const eligableTokens = npcTokens.filter(x => x.name.toLowerCase().includes(table.name.toLowerCase().replace("chatter", "").trim()));
 
-    var tokenIndex = Math.floor((Math.random() * eligableTokens.length) + 0);
-    var token = eligableTokens[tokenIndex];
+    const tokenIndex = Math.floor((Math.random() * eligableTokens.length) + 0);
+    const token = eligableTokens[tokenIndex];
 
     if (token == undefined) return;
-
-    var result = table.roll().results[0].text;
+    let roll = await table.roll();
+    const result = roll.results[0].data.text;
     game.socket.emit("module.npc-chatter", {
-      tokenId: token._id,
+      tokenId: token.data._id,
       msg: result
     });
-    await canvas.hud.bubbles.say(token, result, false);
+    await canvas.hud.bubbles.say(token.data, result, false);
   }
 
   async tokenChatter(token) {
-    var tables = this.getChatterTables();
+    const tables = this.getChatterTables();
 
-    var eligableTables = tables.filter(x => token.name.toLowerCase().includes(x.name.toLowerCase().replace("chatter", "").trim()));
+    const eligableTables = tables.filter(x => token.name.toLowerCase().includes(x.name.toLowerCase().replace("chatter", "").trim()));
 
     if (eligableTables.length == 0) return;
 
-    var tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
-    var table = eligableTables[tableIndex];
-
-    var result = table.roll().results[0].text;
+    const tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
+    const table = eligableTables[tableIndex];
+    let roll = await table.roll();
+    const result = roll.results[0].data.text;
     game.socket.emit("module.npc-chatter", {
-      tokenId: token._id,
+      tokenId: token.data._id,
       msg: result
     });
-    await canvas.hud.bubbles.say(token, result, false);
+    await canvas.hud.bubbles.say(token.data, result, false);
   }
 
   async selectedChatter() {
-    var tables = this.getChatterTables();
+    const tables = this.getChatterTables();
 
-    var npcTokens = canvas.tokens.controlled;
+    const npcTokens = canvas.tokens.controlled;
 
-    var eligableTables = tables.filter(x => npcTokens.filter(t => x.name.toLowerCase().includes(t.name.toLowerCase().replace("chatter", "").trim()) > 0));
+    const eligableTables = tables.filter(x => npcTokens.filter(t => x.name.toLowerCase().includes(t.name.toLowerCase().replace("chatter", "").trim()) > 0));
 
     if (eligableTables.length == 0) return;
 
-    var tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
-    var table = eligableTables[tableIndex];
+    const tableIndex = Math.floor((Math.random() * eligableTables.length) + 0);
+    const table = eligableTables[tableIndex];
 
-    var eligableTokens = npcTokens.filter(x => x.name.toLowerCase().includes(table.name.toLowerCase().replace("chatter", "").trim()));
+    const eligableTokens = npcTokens.filter(x => x.name.toLowerCase().includes(table.name.toLowerCase().replace("chatter", "").trim()));
 
-    var tokenIndex = Math.floor((Math.random() * eligableTokens.length) + 0);
-    var token = eligableTokens[tokenIndex];
-
-    var result = table.roll().results[0].text;
+    const tokenIndex = Math.floor((Math.random() * eligableTokens.length) + 0);
+    const token = eligableTokens[tokenIndex];
+    let roll = await table.roll();
+    const result = roll.results[0].data.text;
     game.socket.emit("module.npc-chatter", {
       tokenId: token.id,
       msg: result
